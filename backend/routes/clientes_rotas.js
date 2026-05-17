4441247aeb27919d8e33a3d3b0f10d7690a0a502
Goncalo Sousa <goncalosousa807@gmail.com>
2026-05-17 20:18:13 +0100

    Fix conflitos

--- backend/routes/clientes_rotas.js
+++ backend/routes/clientes_rotas.js
@@ -11,12 +11,6 @@ const {
 
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
-
-
-// CORREÇÃO 2: Usar essas funções limpas em cada rota
->>>>>>> cc09b54bd5f894775bd1cf4075d26ebc772ffcf3
 router.get("/clientes", listarClientes);
 router.get("/clientes/id/:id", obterClienteByID);
 router.get("/clientes/nif/:nif", obterClienteByNif);
